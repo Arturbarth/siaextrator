103262f510bd95170fdc0ae533e71cbf479d83c2
Artur Barth <arturbarth@gmail.com>
2025-08-26 22:04:26 -0300

    SIA Extrator Commit Inicial

---
+++ src/main/java/br/com/arturbarth/siaextrator/SiaextratorApplication.java
@@ -0,0 +1,13 @@
+package br.com.arturbarth.siaextrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiaextratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiaextratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/arturbarth/siaextrator/SiaextratorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arturbarth.siaextrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiaextratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
